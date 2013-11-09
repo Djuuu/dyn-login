37a79a983cca0b06c414fc0479f7e4c1233d3e14
Djuuu <julien.tardot@gmail.com>
2013-11-10 00:34:09 +0100

    Refactoring, phpdoc, misc.

--- dynlogin.php
+++ dynlogin.php
@@ -30,37 +30,17 @@ $ACCOUNT_URL = "https://account.dyn.com/";
 
 // Functions **********************************************************************
 
-function getRequest($url) {
-    global $CKFILE;
-
-    $ch = curl_init($url);
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    //curl_setopt($ch, CURLOPT_CAINFO, $CURLOPT_CAINFO);
-    curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-
-    curl_setopt ($ch, CURLOPT_COOKIEJAR, $CKFILE);
-    curl_setopt ($ch, CURLOPT_COOKIEFILE, $CKFILE);
-
-    $response = curl_exec($ch);
-    $error = curl_error($ch);
-
-    curl_close($ch);
-
-    if ($response === false) {
-        throw new Exception($error);
-    }
-
-    return $response;
-}
-
-function postRequest($url, $form)
+/**
+ * Sends a web request (GET by default, POST if $post is provided):
+ * If $post is provided
+ * @param string $url
+ * @param array  $post
+ * @return mixed
+ */
+function curlRequest($url, $post=array())
 {
     global $CKFILE;
 
-    /*Initialisation de la ressource curl*/
     $ch = curl_init($url);
 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -72,8 +52,10 @@ function postRequest($url, $form)
     curl_setopt ($ch, CURLOPT_COOKIEJAR, $CKFILE);
     curl_setopt ($ch, CURLOPT_COOKIEFILE, $CKFILE);
 
-    curl_setopt($ch, CURLOPT_POST,true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $form);
+    if (!empty($post)) {
+        curl_setopt($ch, CURLOPT_POST,true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    }
 
     $response = curl_exec($ch);
     $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
@@ -88,9 +70,14 @@ function postRequest($url, $form)
     return $response;
 }
 
+/**
+ * Gets the login page and parses it to extract the login form data
+ * @param string $url
+ * @return array An array corresponding to the login form inputs
+ */
 function getLoginForm($url)
 {
-    $response = getRequest($url);
+    $response = curlRequest($url);
 
     $doc = new DOMDocument();
     $doc->loadHTML($response);
@@ -111,33 +98,32 @@ function getLoginForm($url)
 
 // Procedural **********************************************************************
 
-
 // Clear cookie
-
 if (file_exists($CKFILE)) {
     unlink($CKFILE);
 }
 
 $form = getLoginForm($LOGIN_URL);
 
+// Fill in the credentials
 $form['username'] = $USERNAME;
 $form['password'] = $PASSWORD;
 
 // Post login form with credentials
-$postResult = postRequest($LOGIN_URL, $form);
+$postResult = curlRequest($LOGIN_URL, $form);
 
 if ($postResult === false) {
     return 2;
 }
 
 // Post OK, check account page
-$response = getRequest('https://account.dyn.com/');
+$response = curlRequest($ACCOUNT_URL);
 $logged_in = preg_match('/Welcome&nbsp;<b>'.strtolower($USERNAME).'<\/b>/', $response);
 
 if (php_sapi_name() != "cli") {
     echo $response;
 } else {
-    echo ($logged_in) ? "Log in successful !\n" : "Log in failed :-(\n";
+    echo ($logged_in) ? "Login successful !\n" : "Log in failed :-(\n";
 }
 
 return ($logged_in) ? 0 : 3;
